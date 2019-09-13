301f755e81be458c5ddf32357371979ca22dbaac
MGilB <migil@uc.cl>
2019-09-12 23:44:18 -0300

    Pagina lista, faltan consultas

---
+++ App/consultas/consulta_3.php
@@ -0,0 +1,12 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+  #Consultaaaa
+
+  ?>
+
+<?php include('../templates/footer.html'); ?>
---
+++ App/index.php
@@ -0,0 +1,58 @@
+<?php include('templates/header.html');   ?>
+
+<body>
+  <h1 align="center">Entrega 2</h1>
+
+  <br>
+
+  <h3 align="center"> 1. Centrales Termoelectricas</h3>
+  
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center"> 2. Vertederos de la Región Metropolitana</h3>
+
+  
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center"> 3. Recursos asociados a minas, entre las siguientes fechas</h3>
+
+  <form align="center" action="consultas/consulta_altura.php" method="post">
+      Desde:
+      <input type="text" name="Desde">
+      <br/>
+      Hasta:
+      <input type="text" name="Hasta">
+      <br/><br/>
+      <input type="submit" value="Buscar">
+  </form>
+
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center">4. Regiones con algún recurso vigente</h3>
+
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center">5. Proyectos por socio</h3>
+
+  
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center">6. Proyectos en operacion y con recursos aprobados para ellos</h3>
+
+
+  <br>
+  <br>
+  <br>
+  <br>
+</body>
+</html>
