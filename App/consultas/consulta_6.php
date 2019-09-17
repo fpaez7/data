826185939e76f663ffa2c8127033db9a063a0bdd
MGilB <migil@uc.cl>
2019-09-17 00:31:25 -0300

    Consulta 6 lista

--- App/consultas/consulta_6.php
+++ App/consultas/consulta_6.php
@@ -5,12 +5,35 @@
 <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
-  $query = " SELECT Proyectos.name
-  FROM Proyectos, recurso_proyecto, Recursos
-  WHERE Recursos.status = 'aprovado'
-  AND Proyectos.Pid = recurso_proyecto.Pid
-  AND RECURSOS.Rid = recurso_proyecto.Rid;"
-
+  $query = "SELECT proyectos.pid, proyectos.tipo, proyectos.nombre,
+  proyectos.latitud, proyectos.longitud, proyectos.fecha_apertura FROM proyectos, recurso_proyecto, recursos_cerrados
+  WHERE recursos_cerrados.status = 'aprobado' AND
+  recursos_cerrados.rid = recurso_proyecto.rid AND
+  proyectos.pid = recurso_proyecto.pid AND proyectos.operativo = 1 
+  GROUP BY proyectos.pid 
+  ORDER BY proyectos.pid ASC";
+  $result = $db -> prepare($query);
+  $result -> execute();
+  $proyectos = $result -> fetchAll();
   ?>
+
+  <table align="center">
+    <tr>
+      <th>PID</th>
+      <th>Tipo</th>
+      <th>Nombre</th>
+      <th>Latitud</th>
+      <th>Longitud</th>
+      <th>Fecha Apertura</th>
+    </tr>
+  
+      <?php
+        foreach ($proyectos as $p) {
+          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td>
+          <td>$p[3]</td><td>$p[4]</td><td>$p[5]</td></tr>";
+      }
+      ?>
+      
+  </table>
 
 <?php include('../templates/footer.html'); ?>
