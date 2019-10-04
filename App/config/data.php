ddabf0bd0b8fbc23e187d0d4b39269d603d53b5e
fpaez7 <fpaez1@uc.cl>
2019-10-04 10:10:46 -0300

    Conectandose a dos bases de datos

--- App/config/data.php
+++ App/config/data.php
@@ -1,5 +1,8 @@
 <?php
-$user = 'grupo39';
-$password = 'gauchos39';
-$databaseName = 'grupo39';
+$user1 = 'grupo39';
+$password1= 'gauchos39';
+$databaseName1 = 'grupo39';
+$user2 = 'grupo54';
+$password2= 'cualari8944';
+$databaseName2 = 'grupo54';
 ?>
