1779474da150d804ad51922d55c6783dd810a56a
fpaez7 <fpaez1@uc.cl>
2019-10-04 12:38:11 -0300

    Creo que la cague

--- App/config/conexion.php
+++ App/config/conexion.php
@@ -3,17 +3,17 @@
     #Pide las variables para conectarse a la base de datos.
     require('data.php');
     # Se crea la instancia de PDO
-    $db1 = new PDO("pgsql:dbname=$databaseName1;host=localhost;port=5432;user=$user1;password=$password1");
+    $db1 = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos grupo 56: $e";
   }
-  try {
-    #Pide las variables para conectarse a la base de datos.
-    require('data.php');
-    # Se crea la instancia de PDO
-    $db2 = new PDO("pgsql:dbname=$databaseName2;host=localhost;port=5432;user=$user2;password=$password2");
-  } catch (Exception $e) {
-    echo "No se pudo conectar a la base de datos grupo 49: $e";
-  }
+#  try {
+#    #Pide las variables para conectarse a la base de datos.
+#    require('data.php');
+#    # Se crea la instancia de PDO
+#    $db2 = new PDO("pgsql:dbname=$databaseName2;host=localhost;port=5432;user=$user2;password=$password2");
+#  } catch (Exception $e) {
+#    echo "No se pudo conectar a la base de datos grupo 49: $e";
+#  }
 
 ?>
--- App/config/data.php
+++ App/config/data.php
@@ -1,8 +1,5 @@
 <?php
-$user1 = 'grupo39';
-$password1= 'gauchos39';
-$databaseName1 = 'grupo39';
-$user2 = 'grupo54';
-$password2= 'cualari8944';
-$databaseName2 = 'grupo54';
+$user = 'grupo39';
+$password= 'gauchos39';
+$databaseName = 'grupo39';
 ?>
